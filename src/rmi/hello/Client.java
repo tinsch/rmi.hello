a5e6f4b2191d3376d00189e326bc865efdd4e552
Malwine <malwine@mailbox.org>
2016-11-23 14:49:26 +0100

    Add some comments

--- src/rmi/hello/Client.java
+++ src/rmi/hello/Client.java
@@ -5,7 +5,7 @@ import java.rmi.Naming;
 public class Client {
 	public static void main(String[] args) {
 		try {
-			HelloWorld g = (HelloWorld) Naming.lookup("rmi://localhost:1099/HelloWorld");
+			HelloWorld g = (HelloWorld) Naming.lookup("rmi://localhost:1099/HelloWorld"); //Proxy-Object
 			System.out.println(g.sayHello("malwine"));
 		} catch (Exception e) {
 			System.out.println(e);
--- src/rmi/hello/Server.java
+++ src/rmi/hello/Server.java
@@ -9,9 +9,9 @@ public class Server {
 	public Server() {
 		try {
 			Greeter g = new Greeter();
-			Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-			registry.bind("HelloWorld", g);
-			Naming.rebind("rmi://localhost:1099/HelloWorld", g);
+			Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT); // port is 1099
+			registry.bind("HelloWorld", g); 										   //rmi://localhost:1099/HelloWorld
+			//Naming.rebind("rmi://localhost:1099/HelloWorld", g);                     //central registry
 		} catch (Exception e) {
 			System.out.println(e);
 		}
